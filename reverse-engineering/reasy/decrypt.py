99f60027e9dd5eae0aa2ffa79b016e5b634a1a6c
Christopher Morrison <Chris.Morrison500@gmail.com>
2020-03-27 21:02:09 -0400

    solve-reasy

---
+++ reverse-engineering/reasy/decrypt.py
@@ -0,0 +1,14 @@
+
+flag = b'OSUCTF{R'
+
+data = b"\xe4\xff\xff\xff\x0e\x00\x00\x00"
+data = [byt for byt in data]
+
+for i in range(8):
+    flag += chr(
+        ord(flag[i]) +
+        data[i]:w
+        :q
+
+    )
+
